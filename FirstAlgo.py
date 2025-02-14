673f01501f949c20ad6097c19b447d7af1c77ee0
L31rbag <eg7305@gmail.com>
2025-02-14 10:40:35 +0100

    Amelioration algo
    Ajout et amélioration

--- FirstAlgo.py
+++ FirstAlgo.py
@@ -1,50 +1,52 @@
+import json
 class Noeud:
-    def __init__(self):
-       
+    def __init__(self,valeur):
+        self.valeur = valeur
         self.enfants = {}
-        self.est_fin = False
-        self.mot = ""
+
+    def to_dict(self):
+        return{
+            'valeur': self.valeur,
+            'enfants':{key : enfant.to_dict() for key, enfant in self.enfants.items()}
+        }
+        
+        
 
 class ArbreSuffixes:
     def __init__(self, texte):
-        self.racine = Noeud()
-        self.feuilles = {self.racine}
-        self.suffixes = []
         
-        
-        for caractere in texte + "$":
-            self.ajouter_caractere(caractere)
-            if caractere == "$":
-                
-                self.suffixes = [feuille.mot for feuille in self.feuilles]
-    
-    def ajouter_caractere(self, caractere):
-        nouvelles_feuilles = []
-        
+        self.texte = texte + "$"
+        self.racine = Noeud('')
+        self.construire_arbre()
        
-        nouveau_noeud = Noeud()
-        
-        nouveau_noeud.mot = caractere
-        self.racine.enfants[caractere] = nouveau_noeud
-        nouvelles_feuilles.append(nouveau_noeud)
-        
-      
-        for feuille in self.feuilles:
-            if feuille != self.racine:
-                nouveau_noeud = Noeud()
-                
-                nouveau_noeud.mot = feuille.mot + caractere
-              
-                nouvelles_feuilles.append(nouveau_noeud)
-        
-        self.feuilles = nouvelles_feuilles
+    def inserer_suffixe(self,suffixe):
+        noeud_actu = self.racine
+        print(suffixe)
+        for cc in suffixe :
+            if cc not in noeud_actu.enfants :
+                noeud_actu.enfants[cc]  = Noeud(cc)
+            noeud_actu = noeud_actu.enfants[cc]
+            
     
-    def afficher_suffixes(self):
-      
-        for suffixe in self.suffixes:
-            print(suffixe)
+    def construire_arbre(self):
+
+        for i in range(len(self.texte)):
+            self.inserer_suffixe(self.texte[i:])
 
 
-texte = "banana"
+
+    def to_json(self):  
+        return json.dumps(self.racine.to_dict(),indent=2)
+        
+    
+  
+    
+   
+
+
+texte = "banane"
 arbre = ArbreSuffixes(texte)
-arbre.afficher_suffixes()
+print(arbre.to_json())
+
+with open('arbre_suffixes.json', 'w', encoding='utf-8') as f:
+    f.write(arbre.to_json())
